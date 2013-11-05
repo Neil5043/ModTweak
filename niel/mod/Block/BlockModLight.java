0bcef42eeaaad63a144bb3715c666679826758b7
tterrag1098 <tterrag1098@gmail.com>
2013-11-05 17:34:22 -0500

    Fix light AGAIN

--- niel/mod/Block/BlockModLight.java
+++ niel/mod/Block/BlockModLight.java
@@ -1,20 +1,21 @@
 package niel.mod.Block;
 
 
-import net.minecraft.block.Block;
+import net.minecraft.block.BlockRedstoneLight;
 import net.minecraft.block.material.Material;
 import net.minecraft.client.renderer.texture.IconRegister;
 import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.util.Icon;
+import niel.ModTweak;
 import cpw.mods.fml.relauncher.Side;
 import cpw.mods.fml.relauncher.SideOnly;
 
-public class BlockModLight  extends Block {
-    public BlockModLight(int id, String unlocalizedName) {
-        super(id, Material.wood);
+public class BlockModLight  extends BlockRedstoneLight {
+    public BlockModLight(int id) {
+        super(id, false);
         setHardness(4.0F);
         setCreativeTab(CreativeTabs.tabDecorations);
-        setUnlocalizedName(unlocalizedName);
+        setUnlocalizedName(TweakBlock.BLOCKLIGHT_UNLOC_NAME);
         setLightValue(3F);
     }
     
