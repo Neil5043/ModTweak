41f7c6696f3560a92040f675fd5eece2288654b6
tterrag1098 <tterrag1098@gmail.com>
2013-11-05 16:42:54 -0500

    Light fix.

--- niel/mod/Block/BlockModLight.java
+++ niel/mod/Block/BlockModLight.java
@@ -3,13 +3,31 @@ package niel.mod.Block;
 
 import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
+import net.minecraft.client.renderer.texture.IconRegister;
 import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.util.Icon;
+import cpw.mods.fml.relauncher.Side;
+import cpw.mods.fml.relauncher.SideOnly;
 
 public class BlockModLight extends Block {
     public BlockModLight(int id, String textureName, String unlocalizedName) {
-        super(id, "modtweak" + ":" + textureName, Material.wood);
+        super(id, Material.wood);
         setHardness(4.0F);
         setCreativeTab(CreativeTabs.tabDecorations);
         setUnlocalizedName(unlocalizedName);
+    }
+    
+    @SideOnly(Side.CLIENT)
+    private Icon icon;
+    
+    @SideOnly(Side.CLIENT)
+    public void registerIcons(IconRegister register)
+    {
+    	icon = register.registerIcon("modtweak:" + "light");
+    }
+    
+    public Icon getIcon()
+    {
+    	return icon;
     }
 }
