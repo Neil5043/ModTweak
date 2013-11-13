a86b454daf20fe4210a32e32b4ef153cc39b8363
tterrag1098 <tterrag1098@gmail.com>
2013-11-13 18:34:07 -0500

    derp

--- src/niel/lib/ModLib.java
+++ src/niel/lib/ModLib.java
@@ -5,6 +5,6 @@ public class ModLib
     public static final String MOD_ID = "ModTweak";
     public static final String MOD_NAME = "Mod Tweak";
     public static final String VERSION = "0.0.1";
-    public static final String SERVER_PROXY_CLASS = "niel.proxy.ServerProxy";
+    public static final String SERVER_PROXY_CLASS = "niel.proxy.CommonProxy";
     public static final String CLIENT_PROXY_CLASS = "niel.proxy.ClientProxy";
 }
